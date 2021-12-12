5c87a43ce03fd3a88e5276b8965368045805e43d
Hartye Lilith <hartye.lilith@gmail.com>
2021-12-12 16:21:17 +0000

    New design

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@ function swap() {
   let modal = document.querySelector(".modal");
   modal.classList.toggle("active");
 }
+
+function slide() {
+  let img = document.querySelector(".right-slide-img");
+  let slide_first = document.querySelector(".divOne");
+  let slide_second = document.querySelector(".divTwo");
+  let body = document.querySelector(".body");
+  slide_first.classList.toggle("slide");
+  slide_second.classList.toggle("slide");
+  body.classList.toggle("swap");
+
+  if (img.src.match("svg/right-arrow-alt.svg")) {
+    img.src = "svg/left-arrow-alt.svg";
+  } else {
+    img.src = "svg/right-arrow-alt.svg";
+  }
+}
